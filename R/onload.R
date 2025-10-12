f114870b6d26cae03390cc18f0d9e5ac426e14b0
Jeroen Ooms <jeroenooms@gmail.com>
2025-10-12 13:56:22 +0200

    Bump version

--- R/onload.R
+++ R/onload.R
@@ -5,7 +5,7 @@ ctx <- NULL
   ctx <<- V8::v8()
   wd <- setwd(system.file('js', package = 'minisvg'))
   on.exit(setwd(wd))
-  blob <- readBin('index_bg.wasm', raw(), 1e7)
+  blob <- readBin('index_bg.wasm', raw(), file.info('index_bg.wasm')$size)
   ctx$assign('bytes', blob)
   ctx$eval('var module = new WebAssembly.Module(bytes);')
   ctx$source('resvg-wasm.min.js')
